848e60f47679df09b1911bcd005d36a583b6213d
Christian-Andersen <cdandersen98@gmail.com>
2024-05-16 18:59:42 +1000

    made formatters auto install

--- ftp_backup.sh
+++ ftp_backup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+filename="/tmp/c_$(date +%Y%m%dT%H%M%S).tar"
+
+tar -cpf $filename -C $HOME c
+
 lftp -c "
 open ftp://192.168.1.1:21
-mirror --reverse --delete --parallel /home/christian/c /volume(sda1)/c
+cd volume(sda1)
+put -E $filename
 "
 
--- init.lua
+++ init.lua
@@ -510,6 +510,9 @@ require("lazy").setup({
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				"stylua", -- Used to format Lua code
+				"black",
+				"isort",
+				"clang-format",
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
