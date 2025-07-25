156504a20776b1fbb06443ef3e5bdf7c251a93f9
Christian-Andersen <122653077+Christian-Andersen@users.noreply.github.com>
2025-07-26 08:17:20 +1000

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,31 +5,6 @@ set -gx PATH ~/.local/bin $PATH
 set -gx PATH ~/.cargo/bin $PATH
 set -x UV_MANAGED_PYTHON true
 
-# --- Functions (useful in both interactive and non-interactive contexts if called by scripts) ---
-function a
-    set -l current_dir (pwd)
-    while true
-        set -l venv_path "$current_dir/.venv/bin/activate.fish"
-        if test -f "$venv_path"
-            source "$venv_path"
-            return 0
-        end
-        set -l parent_dir (dirname "$current_dir")
-        if test "$parent_dir" = "$current_dir"
-            return 1
-        end
-        set current_dir "$parent_dir"
-    end
-end
-
-function py
-    uv run -- $argv
-end
-
-function ipy
-    uv run -- ipython -i $argv
-end
-
 function c
     cd ~/c && eza --long --header --group --git
 end
@@ -44,6 +19,9 @@ if status is-interactive
     abbr -a la eza --all --long --header --group --git
     abbr -a lsize eza --all --long --header --group --git --total-size --sort=size
     abbr -a tree eza --tree --long --header --group --git --total-size --sort=size
+    abbr -a a source .venv/bin/activate.fish
+    abbr -a py uv run --
+    abbr -a ipy ipy run -- ipython -i
 
     # Global aliases for help
     abbr -a --position anywhere -- --help '--help | bat -plhelp'
@@ -53,6 +31,6 @@ if status is-interactive
     # These are typically sourced once for the interactive shell's benefit
     uv generate-shell-completion fish | source
     uvx --generate-shell-completion fish | source
-    zoxide init fish | source
+    zoxide init fish --cmd cd | source
     starship init fish | source
 end
