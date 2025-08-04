67e8ac2ce4d80461904b81029c04026f09ffcdab
Christian-Andersen <122653077+Christian-Andersen@users.noreply.github.com>
2025-08-05 08:52:49 +1000

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,25 @@
 set -x EDITOR nvim
 set -x VISUAL nvim
 set -x UV_MANAGED_PYTHON true
-set -gx PATH ~/.local/bin $PATH
-set -gx PATH ~/.cargo/bin $PATH
+fish_add_path --global --move --path ~/.local/bin
+# Homebrew
+set --global --export HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
+
+set --global --export HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar"
+
+set --global --export HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew"
+
+fish_add_path --global --move --path "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin"
+
+if test -n "$MANPATH[1]"
+    set --global --export MANPATH '' $MANPATH
+end
+
+if not contains "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH
+    set --global --export INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH
+end
+
+# Functions
 
 function c
     cd ~/c && eza --long --header --group --git
