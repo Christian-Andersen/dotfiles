d2ecb62c2935d0208b1aed616473efb87a529d17
Christian Andersen <cdandersen98@gmail.com>
2026-01-14 13:59:38 +1100

    added go to last buffer keybind for neovim and made it <leader>b

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -185,6 +185,9 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- Go to last buffer (alternate file)
+vim.keymap.set("n", "<leader>b", "<C-^>", { desc = "Go to last [B]uffer" })
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
