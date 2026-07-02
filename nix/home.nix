6ff84eb33bc9077193fac446898a08cace8c6b11
Christian Andersen <cdandersen98@gmail.com>
2026-07-02 12:12:06 +1000

    fixed 7zip so it uses the proper and updated version

--- nix/home.nix
+++ nix/home.nix
@@ -1,10 +1,16 @@
-{ pkgs, tools, ... }: {
+{ pkgs, tools, ... }: let
+  symlinks = pkgs.runCommand "symlinks" {} ''
+    mkdir -p $out/bin
+    ln -s ${pkgs.neovim}/bin/nvim $out/bin/vi
+    ln -s ${pkgs._7zz}/bin/7zz $out/bin/7z
+  '';
+in {
   programs.bash.enable = false;
 
   home.username = "christian";
   home.homeDirectory = "/home/christian";
 
-  home.packages = tools;
+  home.packages = tools ++ [ symlinks ];
 
   home.stateVersion = "24.11";
 }
