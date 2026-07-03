a6188b59edf05ecaa88bb0ff36232dfed5e52f34
Christian Andersen <cdandersen98@gmail.com>
2026-07-03 10:59:26 +1000

    added go to boilerplate as well as flake to all boilerplate

---
+++ boilerplate/python/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      debugpy = pkgs.python3Packages.debugpy;
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          debugpy
+          just
+          prek
+          ruff
+          ty
+          uv
+        ];
+      };
+    };
+}
