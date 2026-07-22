229b11f646120b5d5348bd7143ccbca162c11ade
Christian Andersen <cdandersen98@gmail.com>
2026-07-23 09:34:49 +1000

    added deno boilerplate

---
+++ boilerplate/deno/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Deno development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          deno
+          git
+          just
+          prek
+        ];
+      };
+    };
+}
